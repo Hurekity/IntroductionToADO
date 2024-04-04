37623ddb280492288ec602b2fdf4a775f408f982
Garawanta Hurekity <Astronomyc9@gmail.com>
2024-04-04 22:32:39 +0900

    Add project files.

---
+++ Library2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;
+                                         Initial Catalog=LibraryPD_321;
+                                         Integrated Security=True;
+                                         Connect Timeout=30;
+                                         Encrypt = False;
+                                         TrustServerCertificate = False;
+                                         ApplicationIntent = ReadWrite;
+                                         MultiSubnetFailover = False";
+            Library library = new Library(connection_string);
+
+            library.InsertAuthor("Abercrombie", "Joe");
+
+            library.SelectAuthors();
+            library.SelectBooks();
+        }
+    }
+}
