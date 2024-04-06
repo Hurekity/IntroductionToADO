435748d3dd34b1c149071328a04d8f53c12f0b01
Garawanta Hurekity <Astronomyc9@gmail.com>
2024-04-06 21:43:09 +0900

    ConnectionString in App.config

--- Library2/Program.cs
+++ Library2/Program.cs
@@ -6,23 +6,24 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Library2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;
-                                         Initial Catalog=LibraryPD_321;
-                                         Integrated Security=True;
-                                         Connect Timeout=30;
-                                         Encrypt = False;
-                                         TrustServerCertificate = False;
-                                         ApplicationIntent = ReadWrite;
-                                         MultiSubnetFailover = False";
-            Library library = new Library(connection_string);
-
-            library.InsertAuthor("Stanne", "Michel") ;
+            //string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;
+            //                             Initial Catalog=LibraryPD_321;
+            //                             Integrated Security=True;
+            //                             Connect Timeout=30;
+            //                             Encrypt = False;
+            //                             TrustServerCertificate = False;
+            //                             ApplicationIntent = ReadWrite;
+            //                             MultiSubnetFailover = False";
+            
+            Library library = new Library();
+            library.InsertAuthor("Hanriksson", "Martin") ;
 
             library.SelectAuthors();
             library.SelectBooks();
