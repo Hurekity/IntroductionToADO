91de44e3777119a14739b2b4f39cdb38eaaa0d5c
Garawanta Hurekity <Astronomyc9@gmail.com>
2024-04-22 16:55:04 +0800

    Library3 DONE

---
+++ Library3/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Library3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBoxQuery = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxQuery
+            // 
+            this.richTextBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxQuery.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxQuery.MaxLength = 256;
+            this.richTextBoxQuery.Multiline = false;
+            this.richTextBoxQuery.Name = "richTextBoxQuery";
+            this.richTextBoxQuery.Size = new System.Drawing.Size(625, 30);
+            this.richTextBoxQuery.TabIndex = 0;
+            this.richTextBoxQuery.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExecute.Location = new System.Drawing.Point(653, 13);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(135, 29);
+            this.buttonExecute.TabIndex = 2;
+            this.buttonExecute.Text = "Execute";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 75);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(776, 363);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(12, 48);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(625, 21);
+            this.comboBoxTables.TabIndex = 4;
+            this.comboBoxTables.SelectedIndexChanged += new System.EventHandler(this.comboBoxTables_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.richTextBoxQuery);
+            this.Name = "MainForm";
+            this.Text = "Library3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxQuery;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+    }
+}
+
---
+++ Library3/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing.Text;
+
+namespace Library3
+{
+    public partial class MainForm : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        SqlDataReader reader;
+        DataTable table;
+        public MainForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            MessageBox.Show(this, connection.ConnectionString, "Connection String", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            richTextBoxQuery.SelectAll();
+            richTextBoxQuery.SelectionAlignment = HorizontalAlignment.Center;
+
+            string command = $@"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
+            SqlCommand cmd = new SqlCommand(command, connection);
+            connection.Open();  
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBoxTables.Items.Add(reader[0]);
+            }
+            connection.Close();
+        }
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            string command = richTextBoxQuery.Text;
+            LoadDataToGrid(command);
+            //SqlCommand cmd = new SqlCommand(cmdLine, connection);
+            //connection.Open();
+            //reader = cmd.ExecuteReader();
+            //table = new DataTable();
+            //for (int i = 0; i < reader.FieldCount; i++)table.Columns.Add(reader.GetName(i));
+            //while(reader.Read())
+            //{
+            //    DataRow row = table.NewRow();
+            //    for (int i = 0; i < reader.FieldCount; i++) row[i] = reader[i];
+            //    table.Rows.Add(row);
+            //}
+            //dataGridView.DataSource = table;
+            //connection.Close();
+        }
+
+        private void comboBoxTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tableName = comboBoxTables.SelectedItem.ToString();
+            string command = $"SELECT * FROM {tableName}";
+            LoadDataToGrid(command);
+           
+        }
+        void LoadDataToGrid (string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            table = new DataTable();
+            for (int i = 0; i < reader.FieldCount; i++)
+                table.Columns.Add(reader.GetName(i));
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader[i];
+                }
+                table.Rows.Add(row);
+            }
+            dataGridView.DataSource = table;
+
+            connection.Close();
+        }
+    }
+}
